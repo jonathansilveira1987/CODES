a91b6b5433106eb2afa9182c935d3041e8286e13
Jonathan <jonathan.silveira@outlook.com>
2023-05-01 16:50:14 -0300

    Codando

--- PYTHON/contagem.py
+++
@@ -1,8 +0,0 @@
-print('\nContagem')
-a1 = int(input('\nPonto Inicial: '))
-a2 = int(input('Ponto Final: '))
-print('\033[0;35m')
-for n in range(a1, a2 + 1):
-    print(n, end=" ")
-print('\033[m')
-print("\n\033[0;32mAcabou!\033[m\n")
