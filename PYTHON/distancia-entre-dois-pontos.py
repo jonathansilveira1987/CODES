8dab76e526c40599c2be4ad7d60aa5aa003b366a
Jonathan <jonathan.silveira@outlook.com>
2023-05-03 00:22:41 -0300

    Codando

---
+++ PYTHON/distancia-entre-dois-pontos.py
@@ -0,0 +1,22 @@
+"""Programa em Python que calcula a distância entre dois pontos, dadas as suas coordenadas. 
+Considerando os pontos A(x1;y1) e B(x2;y2).
+
+x1 é a abscissa de A, y1 é a ordenada de A.
+x2 é a abscissa de B, y2 a ordenada de B
+"""
+
+# Importando biblioteca matemática para a função de raiz quadrada
+from math import sqrt
+
+# Inserido coordenadas dos pontos
+xA = float(input('\nDigite a abscissa do ponto A:'))
+xB = float(input('Digite a abscissa do ponto B:'))
+
+yA = float(input('\nDigite a ordenada do ponto A:'))
+yB = float(input('Digite a abscissa do ponto B:'))
+
+# Calculando a distância
+distAB = sqrt((xA-xB)**2) + ((yA-yB)**2)
+
+# Mostrando resultado
+print('\nA distância entre esses dois pontos é de:', distAB, 'unidades de medida.\n')
