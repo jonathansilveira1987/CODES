8a109df28e2d77e4ae60756bb3a2b7c24796eb39
Jonathan <jonathan.silveira@outlook.com>
2024-03-31 23:55:30 -0300

    Codando

--- PYTHON/002.py
+++ PYTHON/002.py
@@ -7,8 +7,9 @@ data_hora_futura = data_hora_atual + timedelta(days=7)
 
 data_formatada = data_hora_atual.strftime('%d %m %Y')
 hora_formatada = data_hora_atual.strftime('%H %M %S')
-
+print()
 print(data_formatada + " " + hora_formatada)
-
+print()
 print(data_hora_futura.strftime('%d %m %Y') + " " +
       data_hora_futura.strftime('%H %M %S'))
+print()
